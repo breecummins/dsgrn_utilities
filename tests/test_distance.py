ec9237fa4496ef963c6265086b4d47486b200370
Bree Cummins <breecummins@gmail.com>
2021-03-25 12:26:24 -0600

    added another distance test

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -13,3 +13,10 @@ def test2():
     netspec2 = "A : (F) : E\nB : (C) : E\nC : (A)\nE : (E + F)(~B) : E\nF : (~B)"
     assert(symmetric_difference_graph_distance(netspec1,netspec2) == 6)
     assert(symmetric_difference_graph_distance(netspec1,netspec2,normalized=True) == 6 / 28)
+
+
+def test3():
+    netspec1 = "A : (F) : E\nB : (C) : E\nC : (A + E)(~D) : E\nD : (~A)(~E) : E\nE : (F)(~B) : E\nF : (~B) : E"
+    netspec2 = "A : (H) : E\nB : (C) : E\nC : (A)\nE : (E + H)(~B) : E\nH : (~B)"
+    assert(symmetric_difference_graph_distance(netspec1,netspec2) == 14)
+    assert(symmetric_difference_graph_distance(netspec1,netspec2,normalized=True) ==  14 / 28)
